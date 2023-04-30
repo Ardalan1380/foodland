31b9f68b9073ad993d4f41a429880f2fc0077321
Ardalan Hosseini <ardalanh.b8099@gamil.com>
2023-05-01 00:37:21 +0430

    Initial commit

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -16,7 +16,7 @@ const Details = ({data}) => {
 export default Details;
 
 export async function getStaticPaths () {
-    const res = await fetch(`${process.env.BASE_URL}/data`)
+    const res = await fetch("https://api-foodland.vercel.app/data")
     const json = await res.json()
     // test all foods without sliceing
     const data = json.slice(0,10)
@@ -32,7 +32,7 @@ export async function getStaticPaths () {
 
 export async function getStaticProps (context)  {
     const {params : {id}} = context;
-    const res = await fetch(`${process.env.BASE_URL}data/${id}`);
+    const res = await fetch(`https://api-foodland.vercel.app/data/${id}`);
     const data = await res.json()
 
     if(!data.id) {
--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -21,7 +21,7 @@ export default index;
 
 export async function getStaticProps () {
 
-    const res = await fetch(`${process.env.BASE_URL}/data`);
+    const res = await fetch("https://api-foodland.vercel.app/data");
     const data = await res.json()
 
     return {
